83c077efffe163d6ef06bd330690258215be0ff2
al3coding <al3coding@gmail.com>
2021-10-19 13:02:26 +0200

    aggiunta navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,8 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+//= require jquery
+//= require materialize-sprockets
+//= require jquert_ujs
+//= require turbolinks
+//= require_tree .
